2a860e1023126241bbb604c6dd85ca9b67fc9125
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-10-27 01:40:57 +0000

    updates

--- environment.d.ts
+++ environment.d.ts
@@ -10,7 +10,6 @@ declare global
       DNSHOSTS: string;
     }
   }
-}
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
