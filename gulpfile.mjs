7f70918df90bb8b5a7d37acfbc6dc562367101e3
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-10-28 10:18:48 +0000

    scaffolding

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,20 +2,20 @@ import { series } from "gulp";
 import gulp from "gulp";
 import { task, exec, stream } from "gulp-execa";
 
-gulp.task("doc", task("pnpm run doc"));
-gulp.task("compile", task("pnpm run build"));
-gulp.task("format", task("pnpm run format"));
-gulp.task("start", task("pnpm run start"));
-gulp.task("startdev", task("pnpm run start:dev"));
-gulp.task("startdebug", task("pnpm run start:debug"));
-gulp.task("lint", task("pnpm run lint"));
-gulp.task("test", task("pnpm run test"));
-gulp.task("coverage", task("pnpm run test:cov"));
-gulp.task("e2e", task("pnpm run test:e2e"));
+gulp.task("doc", task("npm run doc"));
+gulp.task("compile", task("npm run build"));
+gulp.task("format", task("npm run format"));
+gulp.task("start", task("npm run start"));
+gulp.task("startdev", task("npm run start:dev"));
+gulp.task("startdebug", task("npm run start:debug"));
+gulp.task("lint", task("npm run lint"));
+gulp.task("test", task("npm run test"));
+gulp.task("coverage", task("npm run test:cov"));
+gulp.task("e2e", task("npm run test:e2e"));
 
-gulp.task("clientbuild", task("pnpm run build", {cwd: "./client"}));
-gulp.task("clientformat", task("pnpm run format", {cwd: "./client"}));
-gulp.task("clientlint", task("pnpm run lint", {cwd: "./client"}));
+gulp.task("clientbuild", task("npm run build", {cwd: "./client"}));
+gulp.task("clientformat", task("npm run format", {cwd: "./client"}));
+gulp.task("clientlint", task("npm run lint", {cwd: "./client"}));
 
 gulp.task("default", series("doc", "clientformat", "clientlint", "format", "lint", "clientbuild", "startdev"));
 gulp.task("build", series("doc", "format", "lint", "compile", "clientformat", "clientlint", "clientbuild", "test", "coverage"));
