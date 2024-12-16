49d0e24b5f5f55c0213fba269e199913e72849e2
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-12-16 03:40:13 +0000

    Portainer template Parser

--- src/types/portainer-template.ts
+++ src/types/portainer-template.ts
@@ -1,42 +1,42 @@
-export type Templates = Template[]
+export type Templates = Template[];
 
 export interface PortainerTemplate {
-  version: string
-  templates: Templates
+  version: string;
+  templates: Templates;
 }
 
 export interface Template {
-  categories: string[]
-  description: string
-  env?: Env[]
-  image?: string
-  logo: string
-  name?: string
-  platform: string
-  ports?: string[]
-  restart_policy?: string
-  title: string
-  type: number
-  volumes?: Volume[]
-  note?: string
-  repository?: Repository
-  network?: string
+  categories: string[];
+  description: string;
+  env?: Env[];
+  image?: string;
+  logo: string;
+  name?: string;
+  platform: string;
+  ports?: string[];
+  restart_policy?: string;
+  title: string;
+  type: number;
+  volumes?: Volume[];
+  note?: string;
+  repository?: Repository;
+  network?: string;
 }
 
 export interface Env {
-  label: string
-  name: string
-  default?: string
-  set?: string
-  description?: string
+  label: string;
+  name: string;
+  default?: string;
+  set?: string;
+  description?: string;
 }
 
 export interface Volume {
-  bind?: string
-  container: string
+  bind?: string;
+  container: string;
 }
 
 export interface Repository {
-  stackfile: string
-  url: string
+  stackfile: string;
+  url: string;
 }
