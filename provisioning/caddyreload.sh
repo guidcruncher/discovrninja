d9c31e58a27f9a49581f8b7d6ba9c083067030fe
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2024-12-18 12:51:02 +0000

    fixs

--- provisioning/caddyreload.sh
+++ provisioning/caddyreload.sh
@@ -4,4 +4,5 @@ if [ ! -f /home/app/config/Caddyfile ]; then
   cp /home/defaults/Caddyfile.default /home/app/config/Caddyfile
 fi
 
+caddy fmt --overwrite /etc/caddy/Caddyfile
 caddy reload --config /etc/caddy/Caddyfile
--- provisioning/dnsmasqreload.sh
+++ provisioning/dnsmasqreload.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
 
-if [ ! -f /home/app/config/Caddyfile ]; then
-  cp /home/defaults/Caddyfile.default /home/app/config/Caddyfile
-fi
-
-caddy reload --config
-
