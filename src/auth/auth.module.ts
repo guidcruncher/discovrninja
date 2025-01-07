d1d89d85b98f08ed310429c972e2358f89d895db
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-01-07 07:44:02 +0000

    fixs

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
+import { APP_GUARD } from "@nestjs/core";
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
-
+import { JwtAuthGuard } from "./jwt-auth.guard";
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
