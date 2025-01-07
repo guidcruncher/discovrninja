38ffbcaa5f2871ddd3d41f2f796672fb17b284e8
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-01-07 06:01:34 +0000

    sith

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,3 +1,4 @@
+import { ServerResponse, IncomingMessage } from 'http';
 import { ExecutionContext,Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
@@ -10,6 +11,23 @@ export class LocalAuthGuard extends AuthGuard("local") {
     super();
   }
 
+handleRequest<TUser = GithubReqUser["user"]>(
+    err: Error | null,
+    user: User | false,
+    _info: never,
+    context: ExecutionContext,
+  ): User | void {
+    const res: ServerResponse = context.switchToHttp().getResponse();
+
+    const clientUrl = this.configService.get("allowedOrigin");
+
+    if (err || !user) {
+      return res.redirect(clientUrl + "/login");
+    }
+
+    return user;
+  }
+
   canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
@@ -18,6 +36,9 @@ export class LocalAuthGuard extends AuthGuard("local") {
     if (isPublic) {
       return true;
     }
-    return super.canActivate(context);
+     return super.canActivate(context);
   }
+
+
 }
+
