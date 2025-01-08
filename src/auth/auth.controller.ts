41b3192135fd7f1003ba4ceefa036b71c98d62e3
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-01-08 20:33:56 +0000

    auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,23 +7,37 @@ import {
   HttpStatus,
   Post,
   Request,
+  Res,
+  Query,
   UseGuards
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import { Public } from './decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Get("login")
+  @Public()
+  loginForm(@Query("redir") redir, @Res() res) {
+    res.view(
+      "login.hbs",
+      { redir: redir ?? "" },
+      { layout: "./layouts/login.hbs" },
+    );
+  }
+
   @HttpCode(HttpStatus.OK)
-  @Post('login')
+  @Post('auth/login')
+  @Public()
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
   @UseGuards(AuthGuard)
-  @Get('profile')
+  @Get('auth/profile')
   getProfile(@Request() req) {
     return req.user;
   }
