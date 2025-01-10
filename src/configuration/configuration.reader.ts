107aa9c6884f05e5380b356464b593677bf10bba
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-01-10 04:07:02 +0000

    refactor

---
+++ src/configuration/configuration.reader.ts
@@ -0,0 +1,43 @@
+import { Logger } from "@nestjs/common";
+import { readFileSync } from "fs";e
+import * as fs from "fs";eeassaeqaà
+import * as yaml from "js-yaml";
+import * as path from "path";
+import yargs from "yargs";
+
+/**
+ * Loads configuration from a yaml file
+ */
+export default () => {
+  const log = new Logger("Config");
+  const argv: any = yargs(process.argv.slice(2))
+    .options({ c: { type: "string", alias: "config" } })
+    .parse();
+  let configFolder = "";
+
+  if (!argv.c) {
+    configFolder = path.join(
+      process.env.NODE_CONFIG_DIR ?? __dirname,
+      "config.yaml",
+    );
+  } else {
+    configFolder = path.join(__dirname, argv.c);
+    if (!fs.existsSync(configFolder)) {
+      configFolder = argv.c;
+    }
+  }
+
+  log.debug("Loading configuration from " + configFolder);
+  const configFilename = path.join(configFolder);
+  log.debug("Reading Configuration file " + configFilename);
+
+  if (fs.existsSync(configFilename)) {
+    return yaml.load(readFileSync(configFilename, "utf8")) as Record<
+      string,
+      any
+    >;
+  }
+
+  log.error("ERROR: Configuration file not found.");
+  return null;
+};
