bd211e979fbe2a6b6b05b03b206cd0a03c340982
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-01-11 05:24:42 +0000

    fix

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -1,6 +1,7 @@
 import { Schemas } from "@data/data.schemas";
 import { Module } from "@nestjs/common";
 
+import { CatalogController } from "./catalog.controller";
 import { ComposeController } from "./compose.controller";
 import { ComposeService } from "./compose.service";
 import { PortainerController } from "./portainer.controller";
@@ -8,7 +9,7 @@ import { PortainerService } from "./portainer.service";
 
 @Module({
   imports: [Schemas.CompileModels()],
-  controllers: [PortainerController, ComposeController],
+  controllers: [PortainerController, ComposeController, CatalogController],
   providers: [PortainerService, ComposeService],
   exports: [ComposeService, PortainerService],
 })
