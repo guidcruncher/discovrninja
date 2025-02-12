2b0ab970d6fc2e4bee3f9df88b5e0730ee5bd73f
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-02-12 12:20:26 +0000

    Fixed gulpfile

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -42,7 +42,7 @@ gulp.task("formatjs", task('npx js-beautify -r -s 2 -n -w 0 --type js ./src/clie
 gulp.task("format", series(task("npm run format"), "formatcss", "formathbs", "formathtml"));
 gulp.task("startdev", task("npm run start:dev"));
 gulp.task("uglify", series(task("npm run uglifycss"), task("npm run uglify"), task("npx js-beautify -r -s 2 -n -w 0 --type js ./src/client/public/js/main.js")));
-gulp.task("css", series(task("npm run uglifycss")));
+gulp.task("css", series(task('npx js-beautify -r -s 2 -n -w 0 --type css ./src/client/**/*.css'), task("npm run uglifycss")));
 gulp.task("js", series( "templates", "formatjs", "uglify"));
 gulp.task("lint", task("npm run lint"));
 gulp.task("lint-client", task("npm run lint-client"));
@@ -58,7 +58,7 @@ gulp.task(
     "prebuild",
     "format",
     "lint",
-    "lint-client"
+    "lint-client",
     "js",
     "compile",
   ),
