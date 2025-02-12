ebf934429407a57b137a2edc58f445117d6c9736
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-02-12 05:33:52 +0000

    Build and lint

--- src/main.ts
+++ src/main.ts
@@ -54,8 +54,7 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const clientBase =
-    process.env.CLIENT_BASE ?? path.join(__dirname, "client");
+  const clientBase = process.env.CLIENT_BASE ?? path.join(__dirname, "client");
 
   const log = new Logger("Bootstrap");
   const config: ConfigService = app.get(ConfigService);
