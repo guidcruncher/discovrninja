0fa9e412d0c6021da4a85cfc314d31f986b64a69
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-02-12 01:41:19 +0000

    tweaked eirectory layout

--- buildscripts/mergehelpers.sh
+++ buildscripts/mergehelpers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in ./client/helpers/*.js; do
+for file in ./src/client/helpers/*.js; do
 name="$(basename $file)"
 namenoext="${name::-3}"
 js="$(sed $file -e "s/module.exports = (Handlebars) => {/function _$namenoext(Handlebars) {\n/g")"
--- gulpfile.mjs
+++ gulpfile.mjs
@@ -6,10 +6,10 @@ import wrap from 'gulp-wrap';
 import declare from 'gulp-declare';
 import concat from 'gulp-concat';
 import change from 'gulp-change';
-// import Handlebars from './client/public/js/lib/06-helper.js';
+// import Handlebars from './src/client/public/js/lib/06-helper.js';
 
 gulp.task('templates', function() {
-return gulp.src(['./client/views/partials/*.hbs', './client/views/browser/*.hbs'])
+return gulp.src(['./src/client/views/partials/*.hbs', './src/client/views/browser/*.hbs'])
     .pipe(handlebars()) //{ handlebars: Handlebars}))
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
     .pipe(declare({
@@ -17,11 +17,11 @@ return gulp.src(['./client/views/partials/*.hbs', './client/views/browser/*.hbs'
       noRedeclare: true, // Avoid duplicate declarations
     }))
     .pipe(concat('05-templates.js'))
-    .pipe(gulp.dest('./client/public/js/lib/'));
+    .pipe(gulp.dest('./src/client/public/js/lib/'));
 });
 
 gulp.task('partials', function() {
-  return gulp.src(['./client/views/partials/*.hbs'])
+  return gulp.src(['./src/client/views/partials/*.hbs'])
     .pipe(handlebars())
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
     .pipe(declare({
@@ -29,19 +29,19 @@ gulp.task('partials', function() {
       noRedeclare: true, // Avoid duplicate declarations
     }))
     .pipe(concat('04-partials.js'))
-    .pipe(gulp.dest('./client/public/js/lib/'));
+    .pipe(gulp.dest('./src/client/public/js/lib/'));
 });
 
 gulp.task("helpers",  task("npm run helpers"));
 gulp.task("prebuild", task("npm run prebuild"));
 gulp.task("compile", task("npm run build"));
-gulp.task("formathtml", task("npx js-beautify -r --templating handlebars -s 2 -n -w 0 --type html -j ./client/**/*.html"));
-gulp.task("formathbs", task("npx js-beautify -r --templating handlebars -s 2 -n -w 0 --type html -j ./client/views/**/*.hbs"));
-gulp.task("formatcss", task('npx js-beautify -r -s 2 -n -w 0 --type css ./client/**/*.css'));
-gulp.task("formatjs", task('npx js-beautify -r -s 2 -n -w 0 --type js ./client/public/js/lib/*.js ./client/helpers/*.js '));
+gulp.task("formathtml", task("npx js-beautify -r --templating handlebars -s 2 -n -w 0 --type html -j ./src/client/**/*.html"));
+gulp.task("formathbs", task("npx js-beautify -r --templating handlebars -s 2 -n -w 0 --type html -j ./src/client/views/**/*.hbs"));
+gulp.task("formatcss", task('npx js-beautify -r -s 2 -n -w 0 --type css ./src/client/**/*.css'));
+gulp.task("formatjs", task('npx js-beautify -r -s 2 -n -w 0 --type js ./src/client/public/js/lib/*.js ./src/client/helpers/*.js '));
 gulp.task("format", series(task("npm run format"), "formatcss", "formathbs", "formathtml"));
 gulp.task("startdev", task("npm run start:dev"));
-gulp.task("uglify", series(task("npm run uglifycss"), task("npm run uglify"), task("npx js-beautify -r -s 2 -n -w 0 --type js ./client/public/js/main.js")));
+gulp.task("uglify", series(task("npm run uglifycss"), task("npm run uglify"), task("npx js-beautify -r -s 2 -n -w 0 --type js ./src/client/public/js/main.js")));
 gulp.task("js", series( "templates", "formatjs", "uglify"));
 gulp.task("lint", task("npm run lint"));
 gulp.task("lint-client", task("npm run lint-client"));
