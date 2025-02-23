f107c2d73e805a752143c8e72b4af17ffdc32274
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-02-23 21:44:40 +0000

    Added nodemon

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -43,12 +43,6 @@ gulp.task("js", series( "partials", "templates", "uglify"));
 gulp.task("lint", task("npm run lint"));
 gulp.task("lint-client", task("npm run lint-client"));
 
-gulp.task("dev", series(
-task("rm -r -f ./dist"), "prebuild", "js", 
-task("cp -r ./src/client/public/img/ ./dist/client/public/img/"),
-task("cp -r ./src/client/public/weather/ ./dist/client/public/weather/")
-));
-
 gulp.task("dockerbuild", series(task("npm run helpers"), "format" ,"lint","lint-client", task('docker buildx create --use --bootstrap --driver docker-container  --name discovrninjaBuilder'),
   task('docker buildx build . --builder discovrninjaBuilder -t guidcruncher/discovrninja:development  --no-cache --pull --push --platform linux/arm64'),
   task('docker buildx rm discovrninjaBuilder')));
