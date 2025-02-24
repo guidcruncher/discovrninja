e2201aa0c1e75f0e3299f62cda8840b709d7981e
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-02-24 07:06:25 +0000

    fix buikd

--- buildscripts/packagejs.sh
+++ buildscripts/packagejs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cat $(find /home/node/src/client/public/js/lib/ -type f -name "*.js" | sort) >/home/node/dist/client/public/js/main.js
-
-printf "%s\n%s" "$(cat $(find /home/node/src/client/public/js/lib/ -type f -name "*.min.js" | sort))" "$(cat $(find /home/node/src/client/public/js/lib/ -type f -name "*.js" -not -name "*.min.js" | sort) | npx uglifyjs -c -m))" > /home/node/dist/client/public/js/main.min.js
+sed  -e $'$a\\\n' $(find /home/node/src/client/public/js/lib/ -type f -name "*.js" | sort) >/home/node/dist/client/public/js/main.js
+ 
+printf "%s\n%s" "$(sed  -e $'$a\\\n' $(find /home/node/src/client/public/js/lib/ -type f -name "*.min.js" | sort))" "$(sed  -e $'$a\\\n' $(find /home/node/src/client/public/js/lib/ -type f -name "*.js" -not -name "*.min.js" | sort) | npx uglifyjs -c))" > /home/node/dist/client/public/js/main.min.js
 
 
 
--- src/client/public/js/lib/090-helpers.js
+++ src/client/public/js/lib/090-helpers.js
@@ -643,3 +643,61 @@ function _urlencode(Handlebars) {
 };
 _urlencode(Handlebars);
 
+ function _when(Handlebars) {
+
+   Handlebars.registerHelper("when", function(operand_1, operator, operand_2, options) {
+     var operators = {
+         'eq': function(l, r) {
+           return l == r;
+         },
+         'noteq': function(l, r) {
+           return l != r;
+         },
+         'gt': function(l, r) {
+           return Number(l) > Number(r);
+         },
+         'gte': function(l, r) {
+           return Number(l) >= Number(r);
+         },
+         'lt': function(l, r) {
+           return Number(l) < Number(r);
+         },
+         'lte': function(l, r) {
+           return Number(l) <= Number(r);
+         },
+         'or': function(l, r) {
+           return l || r;
+         },
+         'and': function(l, r) {
+           return l && r;
+         },
+         '%': function(l, r) {
+           return (l % r) === 0;
+         },
+         'in': function(l, r) {
+           return (r.split(',').includes(l));
+         },
+         'notin': function(l, r) {
+           return !(r.split(',').includes(l));
+         },
+         'contains': function(l, r) {
+           if (typeof l === "string")
+             return l.includes(r);
+           else
+             return l ? (l[r] ? true : false) : false;
+         },
+         'notcontains': function(l, r) {
+           if (typeof l === "string")
+             return !l.includes(r);
+           else
+             return l ? !(l[r] ? true : false) : false;
+         }
+       },
+       result = operators[operator](operand_1, operand_2);
+
+     if (result) return options.fn(this);
+     else return options.inverse(this);
+   });
+ };
+_when(Handlebars);
+
