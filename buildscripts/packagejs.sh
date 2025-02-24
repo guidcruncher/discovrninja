3667c00abcf2cb9d50c7e800feb1097be8c152ab
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-02-24 06:16:15 +0000

    fixs

--- buildscripts/packagejs.sh
+++ buildscripts/packagejs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cat $(find ./src/client/public/js/lib/ -type f -name "*.js" | sort) >./dist/client/public/js/main.js
+cat $(find /home/node/src/client/public/js/lib/ -type f -name "*.js" | sort) >/home/node/dist/client/public/js/main.js
 
-printf "%s\n%s" "$(cat $(find ./src/client/public/js/lib/ -type f -name "*.min.js" | sort))" "$(cat $(find ./src/client/public/js/lib/ -type f -name "*.js" -not -name "*.min.js" | sort) | npx uglifyjs -c -m))" > ./dist/client/public/js/main.min.js
+printf "%s\n%s" "$(cat $(find /home/node/src/client/public/js/lib/ -type f -name "*.min.js" | sort))" "$(cat $(find /home/node/src/client/public/js/lib/ -type f -name "*.js" -not -name "*.min.js" | sort) | npx uglifyjs -c -m))" > /home/node/dist/client/public/js/main.min.js
 
 
 
